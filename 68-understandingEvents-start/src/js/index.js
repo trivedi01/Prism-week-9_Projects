71eb4ef1dc69709b372bd50ac8a0bbdf58cdb49e
heta <heta.t96@gmail.com>
2023-03-17 18:57:26 +0530

    68-understandingEvent

---
+++ 68-understandingEvents-start/src/js/index.js
@@ -0,0 +1,6 @@
+const morningBtn = document.querySelector('#morning');
+const dayBtn = document.querySelector('#day');
+const afternoonBtn = document.querySelector('#afternoon');
+const eveningBtn = document.querySelector('#evening');
+const nightBtn = document.querySelector('#night');
+const greeting = document.querySelector('#greeting');
